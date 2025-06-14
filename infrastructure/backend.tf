2ba53e71ca1b5feb0c4c15bdcc2e5cd48025a11f
Sugiyama Tsuyoshi <sugiyama@fullmarks.co.jp>
2025-06-14 14:14:00 +0900

    check tfcloud(2)

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,10 +1,10 @@
 # infrastructure/backend.tf
 terraform {
   cloud {
-    organization = "fact-checker"  # 手順1-1で作成したOrganization名
+    organization = "fact-checker"
 
     workspaces {
-      tags = ["fact-checker"]
+      name = "fact-checker-fs"
     }
   }
 }

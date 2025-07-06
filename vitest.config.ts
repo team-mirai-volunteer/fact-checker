114032fab4b46133102eb0fb3989d01b135c8444
SotaYamaguchi <area029.0709@gmail.com>
2025-07-06 14:48:52 +0900

    - package.jsonに`test:watch`スクリプトを追加 - vitest.config.tsのtsconfigをプロジェクトのものにした

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "node",
-    globals: true,
     include: ["src/**/*.test.ts", "scripts/**/*.test.ts"],
+    typecheck: {
+      tsconfig: "./tsconfig.json",
+    },
   },
 });
